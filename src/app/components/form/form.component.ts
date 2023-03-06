fe62bb7fa289bc984034734917f28504e6697010
eyder-suarez <ey_der19@hotmail.com>
2023-03-05 19:09:13 -0500

    Se agregan las validaciones al formulario

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -25,21 +25,36 @@ export class FormComponent implements OnInit  {
     })
 
     this.myForm = this.fb.group({
-      name           : [''],
-      documentType   : [''],
-      numberDocument : [],
-      address        : [''],
-      phone          : [''],
-      vehicleType    : [''],
-      vehicleBrand   : [''],
-      plate          : ['']
+      name           : ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      documentType   : ['', [Validators.required]],
+      numberDocument : [  , [Validators.required]],
+      address        : ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      phone          : ['', [Validators.required]],
+      vehicleType    : ['', [Validators.required]],
+      vehicleBrand   : ['', [Validators.required]],
+      plate          : ['', [Validators.required, Validators.minLength(6)]]
     })
 
+  }
+
+  /**
+   *
+   * Para validar los campos del formulario
+   * @returns
+   */
+  invalidField( field: string ) {
+    return this.myForm.get(field)?.invalid
+            && this.myForm.get(field)?.touched;
   }
 
   save() {
 
     console.log("Formulario", this.myForm.value);
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+    }else {
+      this.myForm.reset()
+    }
 
 
 
