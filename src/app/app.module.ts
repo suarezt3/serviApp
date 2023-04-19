4af9e9f430d7e8312cade651989f4a49f4a1b319
eyder-suarez <ey_der19@hotmail.com>
2023-04-18 19:36:16 -0500

    Se ajusta el idioma de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID  } from '@angular/core'; // LOCALE_ID Para el idioma español
+import { registerLocaleData } from '@angular/common'; //Para el idioma español
+import localeEs from '@angular/common/locales/es'; //Para el idioma español
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -6,7 +10,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { SharedModule } from './shared/shared.module';
+
+
 import { MessageService } from 'primeng/api';
+
+
+registerLocaleData(localeEs, 'es'); // Registra el idioma español
+
 
 @NgModule({
   declarations: [
@@ -19,7 +29,11 @@ import { MessageService } from 'primeng/api';
     ComponentsModule,
     SharedModule,
   ],
-  providers: [MessageService],
+  providers: [
+    MessageService,
+    { provide: LOCALE_ID, useValue: 'es' } // Establece el locale a español
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
